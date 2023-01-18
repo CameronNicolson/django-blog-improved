b138d9efe84d42561e4de21ffdfcc122bdbafbc9
spongycake <spongycake@spongycake.org>
2023-01-18 22:42:52 +0000

    units for multiple users and group pages

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,34 +1,56 @@
 from django.apps import apps
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.test import TestCase, Client
 from blog.models import User, UserProfile
 from django.urls import reverse
+from django.test.utils import override_settings
+from django.conf import settings
 
 class TestProfiles(TestCase):
-    fixtures = ["users.yaml",]
+    fixtures = ["groups.yaml", "users.yaml",]
 
     def setup(self):
         pass
 
-    def test_new_userprofile_status_private_status(self):
+    def test_status_code_choice(self):
+        from blog.models import user_profile_choice_code
+        code = user_profile_choice_code(False)
+        self.assertEquals(
+                code, 
+                2
+        )
+        code = user_profile_choice_code(True)
+        self.assertEquals(
+                code, 
+                1
+        )
+
+    def test_new_userprofile_status_public_status(self):
+        from blog.conf import USER_PUBLIC_PROFILE
+        self.assertEquals(
+                USER_PUBLIC_PROFILE,
+                True
+        )
         user = User.objects.create(
                 username="snowden",
-                password="password123"
+                password="password123",
         )
         user.save()
+        group = Group.objects.get(name="author")
+        group.user_set.add(user)
         profile = UserProfile.objects.get(user=user)
-        # status code *2* represents private
-        self.assertEquals(profile.status, 2)
-
+        # status code *1* represents public
+        self.assertEquals(profile.status, 1)
         client = Client()
-        response = client.get(reverse("author", kwargs={"name": user.username}))
+        response = client.get(reverse("author", kwargs={"group": group.name, "name": user.username}))
         self.assertEquals(
             response.status_code,
-            404
+            200
         )
         self.assertTemplateUsed(
             response,
-            "404.html"
+            "pages/author.html"
         )
 
     def test_nonstaffer_userprofile_change_denied(self):
@@ -105,3 +127,48 @@ class TestProfiles(TestCase):
             superuser.has_perm(f"{app_label}.change_{model_name}"),
             True
         )
+
+    def test_authorpage_multiple_authors_200(self):
+        basic = get_user_model().objects.get(username="basic")
+        journalist = get_user_model().objects.get(username="journalist")
+        author_group, created = Group.objects.get_or_create(name="author")
+        author_group.user_set.add(basic)
+        author_group.user_set.add(journalist)
+        
+        client = Client()
+        response = client.get(reverse("author", kwargs={"group": author_group.name, "name": f"{basic.username},{journalist.username}"}))
+        self.assertEquals(
+            response.status_code,
+            200
+        )
+        self.assertTemplateUsed(
+            response,
+            "pages/author.html"
+        )
+
+
+    def test_authorpage_multiple_authors_404(self):
+        basic = get_user_model().objects.get(username="basic")
+        journalist = get_user_model().objects.get(username="journalist")
+        author_group, created = Group.objects.get_or_create(name="author")
+        author_group.user_set.add(basic)
+        author_group.user_set.add(journalist)
+        journalist_profile = UserProfile.objects.get(user=journalist)
+        journalist_profile.status = 2
+        journalist_profile.save()
+        self.assertEquals(
+                UserProfile.objects.get(user=journalist).status,
+                2
+        )
+        client = Client()
+        response = client.get(reverse("author", kwargs={"group": author_group.name, "name": f"{basic.username},{journalist.username}"}))
+        self.assertEquals(
+            response.status_code,
+            404
+        )
+        self.assertTemplateUsed(
+            response,
+            "404.html"
+        )
+
+
