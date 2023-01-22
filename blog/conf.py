f3842c399bd6b2adf804f689517da05ea9748375
spongycake <spongycake@spongycake.org>
2023-01-22 18:54:08 +0000

    build_groups django admin command
    A Default group is created with permissions to read/write posts. The groupname is "author".
    
    Blog management revolves around having somebody in charge of writing posts and assigning users is useful for this use case.
    
    `python manage.py build_groups`
    
    -f, --force are flags to force rebuilding.

--- blog/conf.py
+++ blog/conf.py
@@ -1,3 +1,4 @@
 from django.conf import settings
 
-USER_PUBLIC_PROFILE = getattr(settings, 'BLOG_USER_PUBLIC_PROFILE', True)
+USER_PUBLIC_PROFILE = getattr(settings, "BLOG_USER_PUBLIC_PROFILE", True)
+AUTHOR_DEFAULT_GROUP = getattr(settings, "BLOG_AUTHOR_DEFAULT_GROUP", "author")
