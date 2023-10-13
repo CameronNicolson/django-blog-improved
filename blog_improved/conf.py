b5c851410ae0c961b2b915963faeac7023424631
spongycake <spongycake@spongycake.org>
2023-10-13 01:50:31 +0100

    added variable HOMEPAGE_LATESTPOSTS_SIZE to conf
    This commit brings a new variable for directing the size of the post list on the homepage.
    
    HOMEPAGE_LATESTPOSTS_SIZE takes any positive integer and the default is 6.

--- blog_improved/conf.py
+++ blog_improved/conf.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 USER_PUBLIC_PROFILE = getattr(settings, "BLOG_USER_PUBLIC_PROFILE", True)
 AUTHOR_DEFAULT_GROUP = getattr(settings, "BLOG_AUTHOR_DEFAULT_GROUP", "author")
+HOMEPAGE_LATESTPOSTS_SIZE = getattr(settings, "BLOG_HOMEPAGE_LATESTPOSTS_SIZE", 6)  
 
 def set_dynamic_settings(settings):
     """
