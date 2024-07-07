64cad4e47447ae0f7baab344e0fbe9e2f69c8b82
Cameron Nicolson <code.cameron@nicolson.scot>
2024-07-07 16:18:21 +0100

    new test_settings.py

---
+++ test_settings.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+
+DEBUG = True
+
+TEST_ROOT = Path(__file__).parent / "tests"
+
+SECRET_KEY = "django-blog-improved"
+
+ALLOWED_HOSTS = ["*"]
+
+SITE_ID = 1
+
+TIME_ZONE="UTC"
+
+ROOT_URLCONF = "tests.test_urls"
+STATIC_URL = "/static/"
+
+LANGUAGE_CODE = 'en-us'
+
+USE_TZ = True
+
+FIXTURE_DIRS = [
+    TEST_ROOT / "fixtures"
+]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "blog_improved"
+]
+
+
+MIDDLEWARE = [
+    "redirects.middleware.RedirectMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+try:
+    from blog_improved.conf import set_dynamic_settings
+except ImportError:
+    pass
+else:
+    set_dynamic_settings(globals())
