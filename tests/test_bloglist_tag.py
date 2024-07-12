93239360ffc9d9ae11ac62c37159ff5bf546efe9
Cameron Nicolson <code.cameron@nicolson.scot>
2024-07-12 22:21:42 +0100

    +unittest bloglist

--- tests/test_bloglist_tag.py
+++ tests/test_bloglist_tag.py
@@ -1,14 +1,15 @@
+from django.db.models.query import QuerySet
 from django.test import TestCase
 from django.template import Context, Template
 from blog_improved.models import Post
+from blog_improved.templatetags.blog_list_tags import QueryCommand
 
 class BlogListTagTest(TestCase):
     fixtures = ["media.yaml", "tags.yaml", "users.yaml", "redirects.yaml", "groups.yaml", "posts.yaml"]
 
     def test_bloglist_tag(self):
-        template_string = '{% load blog_tags %}{% bloglist filter="title=\'Black\'" %}'
+        template_string = '{% load blog_tags %}{% bloglist category="videos,personal skill" %}'
         template = Template(template_string)
         context = Context({})
-        print(context)
         rendered = template.render(context)
         self.assertEqual(True, True)
