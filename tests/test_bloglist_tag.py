386dcca673d5354e3948f7225ffc1cd1de854f6b
Cameron Nicolson <code.cameron@nicolson.scot>
2024-07-20 15:03:46 +0100

    assert empty content returned

--- tests/test_bloglist_tag.py
+++ tests/test_bloglist_tag.py
@@ -1,15 +1,12 @@
-from django.db.models.query import QuerySet
 from django.test import TestCase
 from django.template import Context, Template
-from blog_improved.models import Post
-from blog_improved.templatetags.blog_list_tags import QueryCommand
 
 class BlogListTagTest(TestCase):
     fixtures = ["media.yaml", "tags.yaml", "users.yaml", "redirects.yaml", "groups.yaml", "posts.yaml"]
 
     def test_bloglist_tag(self):
-        template_string = '{% load blog_tags %}{% bloglist category="videos,personal skill" %}'
+        template_string = '{% load blog_tags %}{% bloglist category=5 %}'
         template = Template(template_string)
         context = Context({})
         rendered = template.render(context)
-        self.assertEqual(True, True)
+        self.assertEqual(rendered, "{}")
