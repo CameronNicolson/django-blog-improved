96efcf52b5792796b7a3f45cda7863952a6fa7de
Cameron Nicolson <code.cameron@nicolson.scot>
2024-10-09 20:53:15 +0100

    refactor bloglisttag into a class based tag

---
+++ blog_improved/posts/posts.py
@@ -0,0 +1,85 @@
+from abc import abstractmethod
+from blog_improved.query_request.query import FilterQueryRequest, QueryRequest
+
+class PostList(list):
+    def __init__(self, post_list=None, category=None, date_generated=None, publish_status=None, fetch_command=None, max_num_posts=None):
+        super().__init__(post_list)
+        self._category = category
+        self._date_generated = date_generated
+        self._max_num_posts = max_num_posts
+        self._publish_status = publish_status
+        self._fetch_command = fetch_command
+
+    def fetch_posts(self):
+        self._fetch_command.retrieve()
+
+class IgnoreCase:
+    def __init__(self, ignore_value):
+        self.ignore_value = ignore_value
+
+
+class PostListBuilder:
+    @abstractmethod
+    def categories(self, category_list):
+        pass
+
+    @abstractmethod
+    def max_num_posts(self, n):
+        pass
+    
+    @abstractmethod
+    def publish(self, status):
+        pass
+
+    @abstractmethod
+    def ignored(self, ignore_case_list):
+        pass
+
+class PostListQueryRequest(PostListBuilder):
+
+    def __init__(self):
+        self._categories = list()
+        self._ignored_cases = tuple()
+        self._max_size = 40
+    
+    @property
+    def categories(self):
+        return self._categories
+
+    def set_max_size(self, number):
+        if not isinstance(number, (int, float)):
+            raise TypeError(self.__class__.__name__ + " takes a standard number type.")
+        self._max_size = int(number)
+        return self
+
+    def set_categories(self, categories):
+        if not isinstance(categories, list):
+            raise TypeError("PostListQuerySet setting categories requires a list")
+        self._categories = categories
+        return self
+    
+    @property
+    def ignored_cases(self):
+        return self._ignored_cases
+
+    def set_ignored_cases(self, ignored_cases):
+        if not isinstance(ignored_cases, tuple):
+            raise TypeError("PostListQuerySet setting ignored cases requires a tuple")
+        self._ignored_cases = ignored_cases
+        return self
+
+    def build(self):
+        request = QueryRequest("blog_improved", "Post", [])
+        request = FilterQueryRequest(queryset_request=request, 
+                                        lookup_field="category__name", 
+                                        lookup_value=self.categories,
+                                        inner_join=["category"])
+        for case in self._ignored_cases:
+            lp_field, lp_type, lp_value = case
+            request = FilterQueryRequest(queryset_request=request,
+                                            negate=True, 
+                                            lookup_field=lp_field, 
+                                            lookup_value=lp_value, 
+                                            lookup_type=lp_type)
+        post_list = request.make_request()
+        return PostList(post_list=post_list, fetch_command=request)
--- blog_improved/templatetags/blog_list_tags.py
+++ blog_improved/templatetags/blog_list_tags.py
@@ -1,14 +1,23 @@
 from django import template
+from django.template import Node, TemplateSyntaxError
 from django.db.models.query import QuerySet
 from blog_improved.models import Post, Status
-from blog_improved.utils.strings import convert_str_kwargs_to_list
-from blog_improved.query_request.query import QuerySetRequest, FilterQuerySetRequest
+from blog_improved.utils.strings import split_string, convert_str_kwargs_to_list
+from blog_improved.query_request.query import QueryRequest, FilterQueryRequest
+from classytags.core import Tag, Options
+from classytags.values import StrictStringValue, StringValue, ListValue, DictValue
+from classytags.arguments import MultiKeywordArgument as ClassyTagsMultiKeywordArgument
+from classytags.arguments import Argument, KeywordArgument, StringArgument, MultiValueArgument
+from classytags.values import IntegerValue
+from classytags.utils import TemplateConstant
+from django.template.base import Variable, VariableDoesNotExist
+from blog_improved.posts.posts import PostListQueryRequest
 
 register = template.Library()
 
 bloglist_args_mapping = {
     "category": {
-        "FilterQuerySetRequest": {
+        "FilterQueryRequest": {
             "negate": False,
             "lookup_field": "category",
             "lookup_type": "exact",
@@ -16,7 +25,7 @@ bloglist_args_mapping = {
         }
     },
     "ignore-category": {
-        "FilterQuerySetRequest": {
+        "FilterQueryRequest": {
             "negate": True,
             "lookup_field": "category",
             "lookup_type": "exact",
@@ -24,7 +33,7 @@ bloglist_args_mapping = {
         }
     },
     "visibility": {
-        "FilterQuerySetRequest": {
+        "FilterQueryRequest": {
             "negate": False,
             "lookup_field": "status",
             "lookup_type": "exact",
@@ -35,10 +44,10 @@ bloglist_args_mapping = {
 }
 
 decorator_registry = {
-        "FilterQuerySetRequest": FilterQuerySetRequest
+        "FilterQueryRequest": FilterQueryRequest
 }
 
-def apply_sequence_queryset_request(instance: QuerySetRequest, requests_to_apply:list):
+def apply_sequence_queryset_request(instance: QueryRequest, requests_to_apply:list):
     queryset_request = instance
     for request in requests_to_apply:
         for classname, class_kwargs in request.items():
@@ -57,15 +66,107 @@ def translate_kwargs(tag_kwargs):
             translated_kwargs.append({class_key: new_kwargs})
     return translated_kwargs
 
+class MultiKeywordArgument(ClassyTagsMultiKeywordArgument):
+  #  sequence_class = ListValue
+    def __init__(self, name, default=None, required=True, resolve=True,
+                 defaultkey=None, splitter='=', commakwarg=None):
+        super().__init__(name, default, required, resolve, defaultkey, splitter)
+        self.commakwarg = commakwarg
 
-@convert_str_kwargs_to_list
-def bloglist(*args, **kwargs):
+    def parse_token(self, parser, token):
+         # Call the parent method to get the token parsed as usual
+        options = super().parse_token(parser, token)
+        key, value = options
+
+        # Check if the key is in the list that requires comma-splitting
+        if key in self.commakwarg:
+            if isinstance(value, StringValue):
+                # Split the resolved value and handle the empty case
+                resolved_value = value.resolve(None)
+                if resolved_value:  # Only proceed if resolved_value is not empty or None
+                    list_values = [TemplateConstant(item) for item in resolved_value.split(',')]
+                    # Initialize the ListValue with the first item, if any
+                    template_list = ListValue(list_values[0]) if list_values else ListValue('')
+                    # Append remaining values, if any
+                    template_list.extend(list_values[1:])
+                    # Update options with the new ListValue
+                    options = (key, template_list)
+        return options
+
+    def _is_a_comma_list(self, token):
+        comma_list = str(token)
+        result = True if "," in comma_list else False
+        return result
+
+class DictWithListValue(dict, StringValue):
+    def __init__(self, value):
+        new_dict = value
+        for key, string in value.items():
+            new_dict[key] = list()
+            for s in split_string(string.literal):
+                new_dict[key].append(s)
+#            for split_str in split_string(string.literal):
+            #new_dict[key].append(split_str)
+        dict.__init__(self, new_dict)
+
+    def clean(self, value):
+        #        items = next(iter(value.values()))
+       # if not isinstance(items, list):
+        #    return self.error(value, "clean")
+        return value
+
+    def resolve(self, context):
+        resolved = {}
+        for key, value in self.items():
+            for list_val in value:
+                try:
+                    resolved_value = Variable(list_val) 
+                    resolved_value = resolved_value.resolve(context)
+                except VariableDoesNotExist:
+                    resolved_value = None  # or handle the error as needed
+                resolved[key] = resolved_value
+        return self.clean(resolved)
+
+class BlogListTag(Tag):
+    name = "bloglist"
+    options = Options(
+        MultiKeywordArgument("bloglist_options", resolve=False, required=False, commakwarg=["category"])
+    )
+
+    def __init__(self, parser, tokens):
+        super().__init__(parser, tokens)
+
+    def render(self, context):
+        options = self.kwargs.pop("%s_options" % self.name)
+        options["max_count"] = IntegerValue(options["max_count"])
+        self.kwargs = options
+        return super().render(context)
+
+    def render_tag(self, context, max_count, category):
+        post_list = PostListQueryRequest()\
+                    .set_max_size(max_count)\
+                    .set_categories(category)\
+                    .build()
+        html = "<ul>"
+        for post in post_list:
+            html += "<li>%s" % str(post.title)
+            html += "</li>"
+        html += "</ul>"
+        return html
+
+
+def bloglist(parser, token):
     # add visibility if not provided by tag
     if kwargs.get("visibility") == None:
         kwargs["visibility"] = Status.name_to_id("publish")
- 
+    for key, arg in kwargs.items():
+        BlogListTagParams(key, arg)
+    # OK we have came to the decision to use the register.tag() the parser
+    # make the kwarg into function pointer and its args
+
+
     requests = translate_kwargs(kwargs)
-    initial_request = QuerySetRequest("blog_improved", "Post", [])
+    initial_request = QueryRequest("blog_improved", "Post", [])
     bob = apply_sequence_queryset_request(initial_request, requests) 
     return {} 
 
