d1dcc76d787dedb78905c159c159ca0ae95b2663
Cameron Nicolson <code.cameron@nicolson.scot>
2024-10-09 20:58:12 +0100

    assert html output from bloglisttag

--- tests/test_bloglist_tag.py
+++ tests/test_bloglist_tag.py
@@ -5,8 +5,8 @@ class BlogListTagTest(TestCase):
     fixtures = ["media.yaml", "tags.yaml", "users.yaml", "redirects.yaml", "groups.yaml", "posts.yaml"]
 
     def test_bloglist_tag(self):
-        template_string = '{% load blog_tags %}{% bloglist category=5 %}'
+        template_string = '{% load blog_tags %}{% bloglist max_count="22" category="colors,programming" %}'
         template = Template(template_string)
         context = Context({})
         rendered = template.render(context)
-        self.assertEqual(rendered, "{}")
+        self.assertEqual(rendered, "<ul><li>The color red facts</li><li>The color yellow facts</li><li>My coding project</li><li>Working title</li></ul>")
