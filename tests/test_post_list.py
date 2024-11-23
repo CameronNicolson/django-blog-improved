6345d6216280e2259c1cd9d3cc2ed5445bc72bd1
Cameron Nicolson <code.cameron@nicolson.scot>
2024-11-23 08:01:56 +0000

    assert postlistmarkup's length and html class attributes

--- tests/test_post_list.py
+++ tests/test_post_list.py
@@ -2,6 +2,7 @@ from blog_improved.posts.posts import PostList
 from blog_improved.posts.post_list_markup import PostListMarkup
 from blog_improved.models import Post
 from django.test import TestCase
+from bs4 import BeautifulSoup
 
 class TestBlogList(TestCase):
     fixtures = ["groups.yaml", "users.yaml", "media.yaml", "tags.yaml", "posts.yaml"]
@@ -20,5 +21,21 @@ class TestBlogList(TestCase):
         posts = Post.objects.all().values_list("title", "headline", "author__username", "published_on", "content",)
         plist = PostList(posts)
         markup = PostListMarkup("post-list-test", plist, 3, 3, (50,25,25,))
-        markup.build_post_list()
+        rendered = markup.build_post_list().get_rendered()
+        print(rendered) 
+        soup = BeautifulSoup(rendered, "html.parser")
 
+        # Find all elements with a class attribute containing "col-one-half" twice
+        elements = soup.find_all(lambda tag: tag.has_attr("class") and tag["class"].count("col-one-half") == 1)
+        
+        # Expect 3 col-one-half li items
+        self.assertEqual(len(elements), 3)
+
+        # Find all <ul> elements
+        ul_elements = soup.find_all('ul')
+
+        # Count the number of <li> items in each <ul>
+        for i, ul in enumerate(ul_elements, start=1):
+            li_count = len(ul.find_all('li'))
+            self.assertEqual(i, 1)
+            self.assertEqual(li_count, 9)
