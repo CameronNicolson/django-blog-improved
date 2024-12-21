82c89ff23a30ec74bf1f207371c2b8d3780bfeff
Cameron Nicolson <code.cameron@nicolson.scot>
2024-12-21 16:25:24 +0000

    resolve favicon

--- examples/urls.py
+++ examples/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 from django.http import Http404
 from django.shortcuts import render
 from django.conf import settings 
@@ -38,6 +38,7 @@ def dynamic_template_view(request, *args, **kwargs):
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html", extra_context={"examples": examples})),
+    path("favicon.ico", RedirectView.as_view(url="/static/img/favicon.ico", permanent=True)),
     re_path(r"^.*/(?P<last_part>[^/]+)/?$", dynamic_template_view),
 ]
 
