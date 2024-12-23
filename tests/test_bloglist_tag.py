1903a0d77de4b4ff1c05ead491bea63da8760ca9
Cameron Nicolson <code.cameron@nicolson.scot>
2024-12-23 00:10:43 +0000

    element checking in DOM was made more generic

--- tests/test_bloglist_tag.py
+++ tests/test_bloglist_tag.py
@@ -1,25 +1,50 @@
+import io
+from pathlib import Path
 from django.test import TestCase
 from bs4 import BeautifulSoup
 from django.template import Context, Template
 
-class BlogListTagTest(TestCase):
+class BlogListTagTestCase(TestCase):
     fixtures = ["media.yaml", "tags.yaml", "users.yaml", "redirects.yaml", "groups.yaml", "posts.yaml"]
 
+    def load_fixture(self, filename):
+        file = Path.cwd() / "tests" / "fixtures" / "html" / filename
+        with open(file, "r") as f:
+            return f.read()
+    
+    @staticmethod
+    def count_siblings(parent):
+        count = 0
+        for child in parent:
+            count = count + 1
+        return count
+
+    @staticmethod
+    def count_post_items(parent):
+        count = len(parent.findAll(class_="posts__item"))
+        return count
+    
     def test_bloglist_tag_no_args(self):
         template_string = '{% load blog_tags %}{% bloglist %}'
         template = Template(template_string)
         context = Context({})
         rendered_html = template.render(context)
         rendered = BeautifulSoup(rendered_html, 'html.parser') 
-        rendered_ul = rendered.find("ul", attrs={"id": "bloglist"})
-        ul_exists = True if rendered_ul else False
-        self.assertTrue(ul_exists)
+        rendered_list = rendered.find(id="bloglist")
+        bloglist_exists = True if rendered_list else False
+        self.assertTrue(bloglist_exists)
+        bloglist_item_count = len(rendered_list.find_all(class_="posts__item"))
+        self.assertEqual(bloglist_item_count, 9)
+
+
+    def test_bloglisttag_invalid_negative_max_count(self):
+        template_string = '{% load blog_tags %}{% bloglist max_count="-1" %}'
+        template = Template(template_string)
+        context = Context({})
+        render = template.render(context) 
 
     def test_bloglisttag_multiple_categories(self):
-        expected_html = '''
-<div class="container"><ul id="bloglist" class=""><li class="bloglist__list-item col-one-third"><article class="article article--featured"><h1 class="article__title">Why Python is the most popular programming language</h1><h2 class="article__headline">2024 Developer Survey saw Python claim one of the top three spots for most-admired languages</h2><address class="article__author"><a rel="alice" href="/author/alice" class="article__author-link">alice</a></address><time class="article__time--published-date" datetime="2024-11-26T13:52:00+00:00">26 November 2024</time><a rel="category" class="author__category--link" href="#">programming</a></article></li><li class="bloglist__list-item col-one-third"><article class="article"><h1 class="article__title">The color red facts</h1><address class="article__author"><a rel="basic" href="/author/basic" class="article__author-link">basic</a></address><time class="article__time--published-date" datetime="2024-01-12T11:03:10+00:00">12 January 2024</time><a rel="category" class="author__category--link" href="#">colors</a></article></li><li class="bloglist__list-item col-one-third"><article class="article"><h1 class="article__title">The color yellow facts</h1><h2 class="article__headline">Yellow represents</h2><address class="article__author"><a rel="basic" href="/author/basic" class="article__author-link">basic</a></address><time class="article__time--published-date" datetime="2024-01-11T10:10:10+00:00">11 January 2024</time><a rel="category" class="author__category--link" href="#">colors</a></article></li><li class="bloglist__list-item col-one-third"><article class="article"><h1 class="article__title">My coding project</h1><address class="article__author"><a rel="alice" href="/author/alice" class="article__author-link">alice</a></address><time class="article__time--published-date" datetime="2022-06-02T13:52:00+00:00">02 June 2022</time><a rel="category" class="author__category--link" href="#">programming</a></article></li></ul></div>
-'''
-  
+        expected_html = self.load_fixture("bloglist_multiple_categories.html") 
         template_string = '{% load blog_tags %}{% bloglist max_count="22" category="colors,programming" %}'
         template = Template(template_string)
         context = Context({})
@@ -27,15 +52,17 @@ class BlogListTagTest(TestCase):
         # Parse both HTML strings
         expected = BeautifulSoup(expected_html, 'html.parser')
         rendered = BeautifulSoup(rendered_html, 'html.parser') 
-        expected_ul = expected.find("ul", {"id": "bloglist"})
-        rendered_ul = rendered.find('ul', {'id': 'bloglist'})
-        self.assertTrue(rendered_ul is not None)
+        expected_list = expected.find(id="bloglist")
+        rendered_list = rendered.find(id="bloglist")
+
+        self.assertTrue(expected_list is not None)
+        self.assertTrue(rendered_list is not None)
         # Count the number of <li> elements within the <ul>
-        expected_li_count = len(expected_ul.find_all("li"))
-        rendered_li_count = len(rendered_ul.find_all("li"))
-        self.assertEqual(rendered_li_count, expected_li_count)
-        expected_categories = expected_ul.find_all("a", attrs={"rel": "category"})
-        rendered_categories = rendered_ul.find_all("a", attrs={"rel": "category"})
+        expected_post_count = BlogListTagTestCase.count_post_items(expected_list)
+        rendered_post_count = BlogListTagTestCase.count_post_items(rendered_list)
+        self.assertEqual(rendered_post_count, expected_post_count)
+        expected_categories = expected_list.find_all("a", attrs={"rel": "category"})
+        rendered_categories = rendered_list.find_all("a", attrs={"rel": "category"})
         expected_categories_count = len(expected_categories)        
         rendered_categories_count = len(rendered_categories)
         self.assertEqual(rendered_categories_count, expected_categories_count)
@@ -44,9 +71,7 @@ class BlogListTagTest(TestCase):
             self.assertTrue(rendered_cat.text in ["colors","programming"])
 
     def test_bloglisttag_featured(self):
-        expected_html = '''
-<div class="posts container"><ul id="featured-news" class="posts__list"><li class="posts__item col-one-third"><article class="article article--featured"><h1 class="article__title">Why Python is the most popular programming language</h1><h2 class="article__headline">2024 Developer Survey saw Python claim one of the top three spots for most-admired languages</h2><address class="article__author"><a rel="alice" href="/author/alice" class="article__author-link">alice</a></address><time class="article__time--published-date" datetime="2024-11-26T13:52:00+00:00">26 November 2024</time><a rel="category" class="author__category--link" href="#">programming</a></article></li></ul></div>
-        '''
+        expected_html = self.load_fixture("bloglist_featured.html")
         template_string = '{% load blog_tags %}{% bloglist name="featured-news" featured="True" max_count="1" %}'
         template = Template(template_string)
         context = Context({})
@@ -55,19 +80,19 @@ class BlogListTagTest(TestCase):
         # Parse both HTML strings
         expected = BeautifulSoup(expected_html, 'html.parser')
         rendered = BeautifulSoup(rendered_html, 'html.parser')
-        # Check the `ul` element with id `bob__list`
-        expected_ul = expected.find('ul', {'id': 'featured-news'})
-        rendered_ul = rendered.find('ul', {'id': 'featured-news'})
-        self.assertTrue(rendered_ul is not None)
+        expected_list = expected.find(id="featured-news")
+        rendered_list = rendered.find(id="featured-news")
+        self.assertTrue(rendered_list is not None)
 
-        # Check the `li` element with class `bob__list-item`
-        expected_li = expected_ul.find("li", {"class": "posts__item"})
-        rendered_li = rendered_ul.find("li", {"class": "posts__item"})
-        self.assertTrue(rendered_li is not None)
+        expected_post = expected_list.find(class_="posts__item")
+        rendered_post = rendered_list.find(class_="posts__item")
+
+        self.assertTrue(expected_post is not None)
+        self.assertTrue(rendered_post is not None)
 
         # Check the article content
-        expected_article = expected_li.find("article")
-        rendered_article = rendered_li.find("article")
+        expected_article = expected_post.find("article")
+        rendered_article = rendered_post.find("article")
         self.assertTrue(rendered_article is not None)
 
         self.assertTrue("article--featured" in expected_article.attrs["class"])
@@ -81,25 +106,28 @@ class BlogListTagTest(TestCase):
         self.assertEqual(rendered_author['href'], expected_author['href'])
 
     def test_bloglisttag_featured_and_ordinary_posts(self):
-        expected_html = ""
+        expected_html = self.load_fixture("bloglist_mix_feature_ordinary_posts.html")
         template_string = '{% load blog_tags %}{% bloglist name="latest-news" featured="True" featured_count="2" max_count="5" %}'
         template = Template(template_string)
         context = Context({})
         rendered_html = template.render(context)
-
+        
         # Parse both HTML strings
         expected = BeautifulSoup(expected_html, 'html.parser')
         rendered = BeautifulSoup(rendered_html, 'html.parser')
+
         # Check the `ul` element with id `latest-news__list`
-        expected_ul = expected.find('ul', {'id': 'latest-news'})
-        rendered_ul = rendered.find('ul', {'id': 'latest-news'})
+        expected_list = expected.find(id="latest-news")
+        rendered_list = rendered.find(id="latest-news")
 
-        self.assertTrue(rendered_ul is not None)
-        #expected_li_count = len(expected_ul.find_all("li"))
-        rendered_li_count = len(rendered_ul.find_all("li"))
-        self.assertEqual(rendered_li_count, 5) 
+        self.assertTrue(expected_list is not None)
+        self.assertTrue(rendered_list is not None)
+        expected_post_count = BlogListTagTestCase.count_post_items(expected_list)
 
-        rendered_featured_count = len(rendered_ul.find_all("article", attrs={"class": "article--featured"}))
+        rendered_post_count = BlogListTagTestCase.count_post_items(rendered_list)
+        self.assertEqual(rendered_post_count, 5) 
+
+        rendered_featured_count = len(rendered_list.find_all("article", attrs={"class": "article--featured"}))
 
         self.assertEqual(rendered_featured_count, 2) 
 
@@ -113,11 +141,11 @@ class BlogListTagTest(TestCase):
         # Parse both HTML strings
         rendered = BeautifulSoup(rendered_html, 'html.parser')
         # Check the `ul` element with id `latest-news__list`
-        rendered_ul = rendered.find('ul', {'id': 'latest-news'})
+        rendered_list = rendered.find(id="latest-news")
 
-        self.assertTrue(rendered_ul is not None)
-        rendered_li_count = len(rendered_ul.find_all("li"))
-        self.assertEqual(rendered_li_count, 2) 
-        rendered_featured_count = len(rendered_ul.find_all("article", attrs={"class": "article--featured"}))
+        self.assertTrue(rendered_list is not None)
+        rendered_posts_count = BlogListTagTestCase.count_post_items(rendered_list)
+        self.assertEqual(rendered_posts_count, 9) 
+        rendered_featured_count = len(rendered_list.find_all("article", attrs={"class": "article--featured"}))
 
         self.assertEqual(rendered_featured_count, 2) 
