7f746b803358549c64d44d1537bec0fa75d42468
Cameron Nicolson <code.cameron@nicolson.scot>
2025-01-29 11:54:49 +0000

    make api a modular app

--- blog_improved/apps.py
+++ blog_improved/apps.py
@@ -1,4 +1,10 @@
+from importlib import import_module 
 from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = "blog_improved"
+
+    def ready(self):
+        modules = ["blog_improved.api"]
+        for module in modules:
+            imported = import_module(module)
