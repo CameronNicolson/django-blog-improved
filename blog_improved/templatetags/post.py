486046d6b46959f3810fd3798df4db750278873d
Cameron Nicolson <code.cameron@nicolson.scot>
2025-02-13 20:44:24 +0000

    id post tag argument directy accepts post primary key integer

--- blog_improved/templatetags/post.py
+++ blog_improved/templatetags/post.py
@@ -19,7 +19,9 @@ class PostTag(Tag):
     name = "post"
     post_model = post_model
 
-    options = Options()
+    options = Options(
+                Argument("id", resolve=False, required=False),
+            )
 
     def __init__(self, parser, tokens):
         super().__init__(parser, tokens)
@@ -29,10 +31,8 @@ class PostTag(Tag):
         return self.post_model
 
     def render(self, context):
-        try:
-            options = self.kwargs.pop("post_options")
-        except KeyError:
-            options = {}
+        id_value = self.kwargs.get("id")
+        options = self.kwargs.get("post_options", {})
 
         post = context.get("post", {})
 
@@ -41,19 +41,31 @@ class PostTag(Tag):
             options["pre_fetched"] = IntegerValue(TemplateConstant(1))
             self.kwargs = options
             return super().render(context)
-
-        # Continue lookup logic only if title is not found
-        id_value = options.get("id")
-        slug_value = post.get("slug")
         
-        lookup_key, lookup_value = ("id", id_value) if id_value else ("slug", slug_value)
-        if lookup_value is "":
+        # Continue lookup logic only if title is not found
+        options.setdefault("id", id_value)
+        slug_value = options.setdefault("slug", StringValue(
+            TemplateConstant(
+                post.get("slug")
+                )
+        ))
+
+        lookup_key, lookup_value = None, None
+        if id_value.resolve(context):
+            lookup_key, lookup_value = ("id", IntegerValue(id_value))
+        elif slug_value.resolve(context):
+            lookup_key, lookup_value = ("slug", StringValue(slug_value))
+        else:
+            raise TemplateSyntaxError(
+                    f"{sys.modules[__name__].__name__} {self.__class__.__name__}: Neither id nor slug was provided."
+                    )
+        
+        if lookup_value == "":
                 raise TemplateSyntaxError(
                     f"{sys.modules[__name__].__name__} {self.__class__.__name__}: {lookup_key}'s value \"{lookup_value}\" is malformed or missing."
                     )
-            
-
-        lookup = DictValue({lookup_key: TemplateConstant(lookup_value)}) if lookup_value else StringValue(TemplateConstant(None))
+ 
+        lookup = DictValue({lookup_key: lookup_value}) if lookup_value else TemplateConstant(None)
 
         # Store values in options and proceed
         options["lookup"] = lookup
@@ -61,7 +73,7 @@ class PostTag(Tag):
         self.kwargs = options
         return super().render(context)
 
-    def render_tag(self, context, lookup, pre_fetched):
+    def render_tag(self, context, lookup, pre_fetched, id=None, slug=None):
         post = None
 
         if lookup is None and pre_fetched is False:
--- tests/test_post_tag.py
+++ tests/test_post_tag.py
@@ -1,5 +1,6 @@
 import io
 from datetime import datetime, timezone
+from classytags.exceptions import TooManyArguments
 from pathlib import Path
 from django.test import TestCase
 from bs4 import BeautifulSoup
@@ -49,7 +50,7 @@ class PostTagTestCase(TestCase):
                                      hour=11, minute=22, second=33, microsecond=444555, tzinfo=timezone.utc)
         self.assertEqual(article_datetime, expected_datetime)
         rendered_author = rendered.find(class_="article__author")
-        xpected_author = rendered_expected.find(class_="article__author")
+        expected_author = rendered_expected.find(class_="article__author")
         try:
             article_author_text = rendered_author.text  
             expected_author_text = expected_author.text
@@ -62,3 +63,42 @@ class PostTagTestCase(TestCase):
         expected_author = rendered_expected.find(class_="article__content") 
         self.assertTrue(rendered_content)
 
+    def test_post_tag_id_only_arg(self):
+        template_string = '{% load blog_tags %}{% post 14 %}'
+        template = Template(template_string)
+        context = Context({}) 
+        rendered_html = template.render(context)
+        rendered = BeautifulSoup(rendered_html, "html.parser") 
+
+        rendered_article_exists = rendered.find(class_="article")
+        self.assertTrue(rendered_article_exists)
+        self.assertTrue(bool(rendered.find(class_="article__title")))
+        self.assertTrue(bool(rendered.find(class_="article__author")))
+        self.assertTrue(bool(rendered.find(class_="article__category")))
+        self.assertTrue(bool(rendered.find(class_="article__time--published-date")))
+
+    def test_post_tag_no_id_or_slug(self):
+        template_string = '{% load blog_tags %}{% post %}'
+        template = Template(template_string)
+        context = Context({}) 
+        with self.assertRaisesRegex(
+            TemplateSyntaxError,
+            r"Neither id nor slug was provided\."
+        ):
+            rendered_html = template.render(context) 
+
+    def test_post_tag_invalid_id(self):
+        template_string = '{% load blog_tags %}{% post 999999999 %}'
+        template = Template(template_string)
+        context = Context({}) 
+        with self.assertRaisesRegex(
+            TemplateSyntaxError,
+            r"No matching records found"
+        ):
+            rendered_html = template.render(context) 
+
+    def test_post_tag_multiple_id_args(self):
+        template_string = '{% load blog_tags %}{% post 12 22 hi %}'
+        with self.assertRaises(TooManyArguments):
+            template = Template(template_string)
+ 
