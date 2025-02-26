601512c800fd6000b8a2635cf807cb1a08d7d354
Cameron Nicolson <code.cameron@nicolson.scot>
2025-02-26 00:50:37 +0000

    added endslashes for easier path resolution

--- blog_improved/urls.py
+++ blog_improved/urls.py
@@ -4,8 +4,8 @@ from blog_improved import views
 from blog_improved.feeds import AtomSiteNewsFeed, LatestPostsFeed
 
 urlpatterns = [
-    path("feed/rss", LatestPostsFeed(), name="rss_feed"),
-    path("feed/atom", AtomSiteNewsFeed(), name="atom_feed"),
-    path("<str:group>/<str:name>", views.AuthorPage.as_view(), name="user_profile"),
+    path("feed/rss/", LatestPostsFeed(), name="rss_feed"),
+    path("feed/atom/", AtomSiteNewsFeed(), name="atom_feed"),
+    path("<str:group>/<str:name>/", views.AuthorPage.as_view(), name="user_profile"),
     path("<slug:slug>/", views.PostView.as_view(), name="post_detail"),
 ]
