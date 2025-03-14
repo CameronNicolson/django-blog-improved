9e541f4a1c5ee148ef0d9d84990104dcfae3c1c7
Cameron Nicolson <code.cameron@nicolson.scot>
2025-03-14 05:58:37 +0000

    remove unnecessary imports related to theming

--- blog_improved/apps.py
+++ blog_improved/apps.py
@@ -1,9 +1,7 @@
 from django.apps import AppConfig
 from pathlib import Path
-from blog_improved.themes.settings import load_theme, get_theme
 from blog_improved.conf import get_theme_settings
 from blog_improved.utils.component_loader import get_sgml_generator, get_presentation_strategy
-from blog_improved.utils.theme_intergration  import get_theme_width_map, get_theme_grid, integrate_theme_with_generator
 from django.conf import settings
 from blog_improved import conf
 from blog_improved.formatters.env import Env
